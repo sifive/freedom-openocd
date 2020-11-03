97c93cebad1affc0586b61541ae74b0dca0a1f30
Kevin Mills <kevin.mills@sifive.com>
2020-11-03 13:34:18 -0800

    bump version id for November release

--- Version.mk
+++ Version.mk
@@ -2,4 +2,4 @@
 RISCV_OPENOCD_VERSION := 0.10.0
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_OPENOCD_ID := 2020.09.0-preview1
+FREEDOM_OPENOCD_ID := 2020.11.0
